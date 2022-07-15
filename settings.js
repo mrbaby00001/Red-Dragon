fd9e437c8f2c7bf227036b0de8c96ff5ba1cf41e
mrbaby00001 <109387053+mrbaby00001@users.noreply.github.com>
2022-07-16 02:03:27 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -19,31 +19,31 @@ global.APIKeys = {
 
 //customize settings\\
 
-global.alivemsg = `My name is RED DRAGON🔥
-My Creator is Kumuthu Prabhasha😎` //Costomize Alive Message (type your message in `` )
+global.alivemsg = `My name is chathu baba💃🔥
+My Creator is idu baba` //Costomize Alive Message (type your message in `` )
 
-global.owner = ['+380944814219'] //Owner number in aive msg
-global.premium = ['+380944814219'] //Owner Number info
-global.ownernomer = '+380944814219' //Owner Number <<<
+global.owner = ['+94766660528'] //Owner number in aive msg
+global.premium = ['+94766660528'] //Owner Number info
+global.ownernomer = '+94766660528' //Owner Number <<<
 
-global.ownername = '😎𝙆.𝙋𝙧𝙖𝙗𝙝𝙖𝙨𝙝𝙖' //Owner Name
-global.botname = ' ☬RED༒DRAGON࿐' //Bot Name
+global.ownername = 'chathu baba💃' //Owner Name
+global.botname = ' ☬mr༒baby࿐' //Bot Name
 
 global.button = '🤭Youtube📍' //Costomize A Button Name In Alive Message
-global.btnurl = 'https://youtu.be/E5C-lrl_g9I' //Change Your Button's URL
+global.btnurl = 'http://wa.me/+94777511834?text=°◕𝓗𝓘-යකූ-නම-ගම-කියන්න-සෙවු-කරගන්න' //Change Your Button's URL
 
 global.region = 'India, South India, Kerala' //Country Region
 
-global.packname = '☬RED༒DRAGON࿐' //Bot Pacage Name
-global.author = '😎𝙆.𝙋𝙧𝙖𝙗𝙝𝙖𝙨𝙝𝙖❯❯' //Author Name
+global.packname = '☬idu baba🤓࿐' //Bot Pacage Name
+global.author = 'idu baba🤓' //Author Name
 global.prefa = ['','!','.','😂','❤️','💠'] //Command Prefix eg:(.alive ,alive 😂alive)
 global.sessionName = 'session' //Session Name!
 global.sp = '⭔' //A Charactor
 global.dragonemoji = '🧩' // Menu emoji
-global.footer = '©_Kumuthu.' //In list menu
+global.footer = '©_idu baba🤓.' //In list menu
 global.chatbotpre = 'true'  // Chat bot true/false
 
-global.ig = 'https://github.com/prabhasha2006' //Github Account!
+global.ig = 'http://wa.me/+94777511834?text=°◕𝓗𝓘-යකූ-නම-ගම-කියන්න-සෙවු-කරගන්න' //Github Account!
 
 global.mess = { // COSTOMIZE WITH YOUR LANGUAGE
 	
@@ -54,7 +54,7 @@ global.mess = { // COSTOMIZE WITH YOUR LANGUAGE
     group: 'Feature Used Only For Groups!',
     private: 'This Features Only For Private Chat!',
     bot: 'This Feature Is Only For Bot😝',
-    wait: '👉👈Pleas Wait...',
+    wait: '👉👈Pleas Wait😒...',
     error: 'Error!!! Maybe Api Key Is Expired🤔!',
     endLimit: 'Your Daily Limit Has Expired, The Limit Will Be Reset Every 12 Hours',
 }
